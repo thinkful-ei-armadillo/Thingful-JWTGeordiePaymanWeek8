66c8ee5e2d51159940e328cf3380471ca92b5c60
David Haugen <haugen.dj@gmail.com>
2019-03-05 14:02:30 -0600

    fix logout, add bcrypt

--- thingful-server/src/middleware/basic-auth.js
+++ thingful-server/src/middleware/basic-auth.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcryptjs');
 
 function requireAuth(req, res, next) {
   const authToken = req.get('Authorization') || '';
@@ -24,11 +25,18 @@ function requireAuth(req, res, next) {
     .where({ user_name: tokenUserName })
     .first()
     .then(user => {
-      if (!user || user.password !== tokenPassword) {
+      if (!user) {
         return res.status(401).json({ error: 'Unauthorized request. User credentials incorrect.'});
       }
-      req.user = user;
-      next();
+      return bcrypt.compare(tokenPassword, user.password)
+        .then(passwordsMatch => {
+          if (!passwordsMatch) {
+            return res.status(401).json({ error: 'Unauthorized request' });
+          }
+      
+          req.user = user;
+          next();
+        });    
     })
     .catch(next);
 }
